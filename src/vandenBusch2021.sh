bc319abdb68663b687f867b2a771487d3d6beba8
jlvdb <jlvdb@astro.uni-bonn.de>
2021-02-26 00:20:31 +0100

    copy multinest output to ChainDir

--- src/vandenBusch2021.sh
+++ src/vandenBusch2021.sh
@@ -537,14 +537,10 @@ then
   cat base/plikHM_TTTEEE_lowl_lowE/base_plikHM_TTTEEE_lowl_lowE_?.txt > base_plikHM_TTTEEE_lowl_lowE.txt
   ln -s base/plikHM_TTTEEE_lowl_lowE/base_plikHM_TTTEEE_lowl_lowE.paramnames . 
   #Link the completed goldsets
-  if [ "$BLINDS" == "NONE" ]
-  then
-    BLINDS="UNBLINDED"
-  fi
   for GoldSet in ${GOLDLIST}
   do
-    ln -sf ../COSMOPIPE/GoldSet_${GoldSet}/${STORAGEDIR}/MCMC/output/sci_${BLINDS}/${SURVEY}__HEADER.txt GoldSet_${GoldSet}.txt  
-    ln -sf ../COSMOPIPE/GoldSet_${GoldSet}/${STORAGEDIR}/MCMC/output/sci_${BLINDS}/${SURVEY}__HEADER.paramnames GoldSet_${GoldSet}.paramnames  
+    cp -fv ../COSMOPIPE/GoldSet_${GoldSet}/${STORAGEDIR}/MCMC/output/K1000_UNBLINDED/cosebis/chain/output_multinest_${BLINDS}.txt \
+          GoldSet_${GoldSet}_multinest_${BLINDS}.txt
   done
 fi 
 
