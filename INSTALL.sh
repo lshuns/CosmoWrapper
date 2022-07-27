db1379b043e4b6aa6a69698a6c6261b34af9db15
Jan Luca van den Busch <jlvdb@astro.rub.de>
2022-07-27 12:54:01 +0000

    fixed the installation for R>=4

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,23 +12,23 @@ fi
 
 #Install the required packages 
 R --no-restore --no-save <<EOF
+library('remotes')
 qrequire<-function(...) suppressWarnings(suppressPackageStartupMessages(require(...)))
-require.and.load<-function(name,githubrep,force=FALSE) { 
-  if (!qrequire(name,character.only=TRUE) || force) { 
-    if (!missing(githubrep)) { 
-      devtools::install_github(paste(githubrep,name,sep='/'),upgrade='always')
-    } else { 
-      install.packages(name,repos='https://cloud.r-project.org/')
+require.and.load<-function(name,githubrep,force=FALSE) {
+    if (!qrequire(name,character.only=TRUE) || force) {
+        if (!missing(githubrep)) {
+            remotes::install_github(paste(githubrep,name,sep='/'),upgrade='always')
+        } else {
+            install.packages(name,repos='https://cloud.r-project.org/')
+        }
+        if (grepl('/',name) & !missing(githubrep)) {
+            name<-rev(strsplit(name,'/')[[1]])[1]
+        }
+        if (!qrequire(name,character.only=TRUE)) {
+            stop(paste("Failed to install package",name))
+        }
     }
-    if (grepl('/',name) & !missing(githubrep)) { 
-      name<-rev(strsplit(name,'/')[[1]])[1]
-    }
-    if (!qrequire(name,character.only=TRUE)) { 
-      stop(paste("Failed to install package",name))
-    }
-  }
 }
-require.and.load('devtools') 
 require.and.load('RColorBrewer') 
 require.and.load('matrixStats')
 require.and.load('data.table') 
@@ -37,7 +37,6 @@ require.and.load('FITSio')
 require.and.load('foreach')
 require.and.load('doParallel')
 require.and.load('itertools')
-require.and.load('astro')
 require.and.load('kohonen/kohonen','AngusWright') 
 
 EOF
